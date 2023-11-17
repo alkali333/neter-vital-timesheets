a23040f5385ab4a90efd704e8c14d94b2a6ccb11
alkali333 <jake@alkalimedia.co.uk>
2023-11-17 07:48:21 +0000

    Second commit

--- app/db_functions.py
+++ app/db_functions.py
@@ -1,5 +1,10 @@
 from datetime import datetime, timedelta
+from werkzeug.security import check_password_hash
+
 from sqlalchemy.orm import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy import func
+
 from models import (
     Shift,
     User,
@@ -8,6 +13,35 @@ from sqlalchemy import and_
 
 # Initialize your database session
 # session = Session()
+
+
+# Authenticating user with database
+def authenticate(session, table, email: str, password: str) -> bool:
+    try:
+        user_in_db = session.query(table).filter_by(email=email).one()
+        if check_password_hash(user_in_db.password, password):
+            return True
+    except NoResultFound:
+        pass
+    return False
+
+
+def find_shift_for_user_today(user_id: int, session: Session):
+    # Get the current date, with the time set to midnight
+    today = datetime.utcnow().date()
+
+    # Query for existing shifts for the user that start today
+    existing_shift = (
+        session.query(Shift)
+        .filter(
+            Shift.user_id == user_id,
+            func.date(Shift.date)
+            == today,  # Extracting and comparing only the date part of the datetime
+        )
+        .first()
+    )  # Using first() will return the first match or None if there is no match
+
+    return existing_shift
 
 
 def start_shift(user_id: int, session: Session):
@@ -24,80 +58,79 @@ def start_shift(user_id: int, session: Session):
     session.commit()
 
 
-def start_break(user_id: int, session: Session):
+def start_break(shift_id: int, session: Session):
     """
-    Updates the current shift for the user by setting the status to 'on break' and updating total_worked
-    with the time worked since the last status change.
+    Updates the shift with the provided shift_id for the user by setting the status to 'on break'
+    and updating total_worked with the time worked since the last status change.
     """
-    current_shift = (
-        session.query(Shift)
-        .filter(
-            and_(
-                Shift.user_id == user_id,
-                Shift.end_time.is_(None),
-                Shift.date == datetime.utcnow().date(),
-            )
-        )
-        .one_or_none()
-    )
+    try:
+        # Retrieve the shift with the provided shift_id
+        current_shift = session.query(Shift).filter(Shift.shift_id == shift_id).one()
 
-    if current_shift and current_shift.status == "working":
-        # Calculate the time worked since the last status change
-        worked_time = datetime.utcnow() - current_shift.start_time
-        if current_shift.total_worked:
-            current_shift.total_worked += worked_time
+        # Check if the shift is currently working
+        if current_shift.status == "working":
+            # Calculate the time worked since the last status change
+            worked_time = datetime.utcnow() - current_shift.start_time
+
+            # Update total_worked with the time worked
+            if current_shift.total_worked:
+                current_shift.total_worked += worked_time
+            else:
+                current_shift.total_worked = worked_time
+
+            # Set the status to 'on break'
+            current_shift.status = "on break"
+
+            # Commit the changes to the database
+            session.commit()
         else:
-            current_shift.total_worked = worked_time
-        current_shift.status = "on break"
-        session.commit()
+            print("The shift is not currently in 'working' status.")
+    except NoResultFound:
+        # Handle the case where no shift is found with the given shift_id
+        print(f"No shift found with shift_id {shift_id}")
 
 
-def end_break(user_id: int, session: Session):
+def end_break(int, shift_id: int, session: Session):
     """
-    Updates the current shift for the user by setting the status to 'working' and updating total_break
-    with the time spent on break since the last status change.
+    Updates the shift with the provided shift_id for the user by setting the status to 'working'
+    and updating total_break with the time spent on break since the last status change.
     """
-    current_shift = (
-        session.query(Shift)
-        .filter(
-            and_(
-                Shift.user_id == user_id,
-                Shift.end_time.is_(None),
-                Shift.date == datetime.utcnow().date(),
+    try:
+        # Retrieve the shift with the provided shift_id
+        current_shift = session.query(Shift).filter(Shift.shift_id == shift_id).one()
+
+        # Check if the shift is currently on break
+        if current_shift.status == "on break":
+            # Calculate the break time
+            break_time = datetime.utcnow() - (
+                current_shift.start_time + current_shift.total_worked
             )
-        )
-        .one_or_none()
-    )
 
-    if current_shift and current_shift.status == "on break":
-        # Calculate the break time
-        break_time = datetime.utcnow() - (
-            current_shift.start_time + current_shift.total_worked
-        )
-        if current_shift.total_break:
-            current_shift.total_break += break_time
+            # Update total_break with the break time
+            if current_shift.total_break:
+                current_shift.total_break += break_time
+            else:
+                current_shift.total_break = break_time
+
+            # Set the status to 'working'
+            current_shift.status = "working"
+
+            # Commit the changes to the database
+            session.commit()
         else:
-            current_shift.total_break = break_time
-        current_shift.status = "working"
-        session.commit()
+            print("The shift is not currently in 'on break' status.")
+    except NoResultFound:
+        # Handle the case where no shift is found with the given shift_id
+        print(f"No shift found with shift_id {shift_id}")
 
 
-def end_shift(user_id: int, session: Session):
+def end_shift(shift_id: int, session: Session):
     """
     Updates the current shift for the user by setting the status to 'not working', the end time to the current time,
     and updates total_worked with the time worked since the last status change if the user is coming off a break.
     """
-    current_shift = (
-        session.query(Shift)
-        .filter(
-            and_(
-                Shift.user_id == user_id,
-                Shift.end_time.is_(None),
-                Shift.date == datetime.utcnow().date(),
-            )
-        )
-        .one_or_none()
-    )
+
+    current_shift = session.query(Shift).filter(Shift.shift_id == shift_id).one()
 
     if current_shift:
         current_shift.end_time = datetime.utcnow()
@@ -112,3 +145,35 @@ def end_shift(user_id: int, session: Session):
                 current_shift.total_worked = worked_time
         current_shift.status = "not working"
         session.commit()
+
+    else:
+        print(f"No shift found with shift_id {shift_id}")
+
+
+def resume_shift(shift_id: int, session: Session):
+    """
+    Allows a worker to resume a shift if it was ended prematurely by using the shift_id.
+    """
+    try:
+        # Retrieve the shift with the provided shift_id
+        current_shift = session.query(Shift).filter(Shift.shift_id == shift_id).one()
+
+        # Check if the shift is marked as 'not working' and the end time is today
+        if (
+            current_shift.status == "not working"
+            and current_shift.end_time
+            and current_shift.end_time.date() == datetime.utcnow().date()
+        ):
+            # Reset end_time to None and change status back to 'working'
+            current_shift.end_time = None
+            current_shift.status = "working"
+            session.commit()
+            return True
+        else:
+            # If the shift is not marked as 'not working' or the end time is not today, it cannot be resumed
+            print(f"The shift with shift_id {shift_id} cannot be resumed.")
+    except NoResultFound:
+        # Handle the case where no shift is found with the given shift_id
+        print(f"No shift found with shift_id {shift_id}")
+
+    return False
--- app/main.py
+++ app/main.py
@@ -1,5 +1,14 @@
 import streamlit as st
-from models import User, Shift
+from models import User, Shift, SessionLocal
+from db_functions import (
+    authenticate,
+    find_shift_for_user_today,
+    start_shift,
+    start_break,
+    end_break,
+)
+from datetime import datetime, timezone
+
 
 st.set_page_config(
     page_title="Neter Vital Timesheet",
@@ -11,3 +20,60 @@ st.set_page_config(
 
 
 st.title("Neter Vital Timesheet")
+# Get the current date and time in UTC
+current_utc_datetime = datetime.now(timezone.utc)
+
+# Format the date and time in a nice format, for example: 'YYYY-MM-DD HH:MM:SS'
+formatted_date = current_utc_datetime.strftime("%Y-%m-%d %H:%M:%S")
+
+st.header(formatted_date)
+
+
+# user not logged in
+if "user_id" not in st.session_state:
+    email = st.sidebar.text_input("Email", value="jake@alkalimedia.co.uk")
+    password = st.sidebar.text_input("Password", type="password", value="Love+Code108")
+
+    if st.sidebar.button("Login"):
+        with SessionLocal() as session:
+            authenticated = authenticate(session, User, email, password)
+
+        if authenticated:
+            with SessionLocal() as session:
+                user_in_db = session.query(User).filter_by(email=email).first()
+
+                st.session_state.user_id = user_in_db.user_id
+                # store user name
+                st.session_state.user_name = user_in_db.name
+
+else:
+    st.write(f"Welcome {st.session_state.user_name}")
+
+    with SessionLocal() as session:
+        user_shift_today = find_shift_for_user_today(
+            user_id=st.session_state.user_id, session=session
+        )
+        if user_shift_today:
+            # store the shift ID in a session variable
+            st.session_state.shift_id = user_shift_today.shift_id
+
+            if user_shift_today.status == "working":
+                if st.button(label="Start My Break", use_container_width=True):
+                    with SessionLocal() as session:
+                        start_break(
+                            shift_id=st.session_state.shift_id,
+                            session=session,
+                        )
+                        st.rerun()
+                # need option to end shift
+            elif user_shift_today.status == "on break":
+                st.button(label="End My Break", use_container_width=True)
+
+        else:
+            if st.button(label="Start My Shift", use_container_width=True):
+                with SessionLocal() as session:
+                    start_shift(user_id=st.session_state.user_id, session=session)
+                    st.experimental_rerun()
+
+
+# What happens to an ended shift? Do I need a function to resume it?
